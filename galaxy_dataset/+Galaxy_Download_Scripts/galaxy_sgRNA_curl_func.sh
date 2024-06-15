8dc7befa4a2fe88f2e0f03e5c20a046bf899bef3
Xander <alxxander.nguyen@gmail.com>
2024-06-15 01:55:56 -0700

    updated curl_func

--- galaxy_dataset/+Galaxy_Download_Scripts/galaxy_sgRNA_curl_func.sh
+++ galaxy_dataset/+Galaxy_Download_Scripts/galaxy_sgRNA_curl_func.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sgRNA_galaxy_curl () {
-    local category_label="$1"
+    local dataset_label="$1"
     local sample_name="$2"
     local galaxy_id="$3"
 
@@ -13,12 +13,12 @@ sgRNA_galaxy_curl () {
     echo "$readfile_url"
     echo "$bowtie2_bam_url"
 
-    mkdir -p "../../results/${category_label}/${sample_name}/"
-    mkdir -p "../${category_label}/${sample_name}/"
+    mkdir -p "../../results/${dataset_label}/${sample_name}/"
+    mkdir -p "../${dataset_label}/${sample_name}/"
 
     curl --parallel \
-        -o "../${category_label}/${sample_name}/readfile.zip" "${readfile_url}" \
-        -o "../${category_label}/${sample_name}/bowtie2_bam.zip" "${bowtie2_bam_url}"
+        -o "../${dataset_label}/${sample_name}/readfile.zip" "${readfile_url}" \
+        -o "../${dataset_label}/${sample_name}/bowtie2_bam.zip" "${bowtie2_bam_url}"
 
-    unzip "../${category_label}/${sample_name}/*" -d "../${category_label}/${sample_name}"   
+    unzip "../${dataset_label}/${sample_name}/*" -d "../${dataset_label}/${sample_name}"   
 }
